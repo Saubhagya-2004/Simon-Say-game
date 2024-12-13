f2480400f5fbf31fd4020c3d24ce0b7e2b9ceb81
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-14 01:14:02 +0530

    Changes

---
+++ app.js
@@ -0,0 +1,13 @@
+let gameseq=[];
+let user=[];
+let started=false;
+let level=0;
+document.addEventListener('keypress',function(){
+    if(started==false){
+        alert('gane is started')
+        started=true;
+    }
+    else{
+        
+    }
+})

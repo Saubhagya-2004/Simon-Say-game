08cd2706a04ed7e8ea407e924965a9870c09f39a
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-14 14:15:10 +0530

    changes

--- app.js
+++ app.js
@@ -2,12 +2,51 @@ let gameseq=[];
 let user=[];
 let started=false;
 let level=0;
+let h2=di=document.querySelector('h2');
+let btns=['red','green','yellow','blue'];
 document.addEventListener('keypress',function(){
     if(started==false){
-        alert('gane is started')
+        console.log('game is started')
         started=true;
+        levelup();
     }
-    else{
-        
-    }
-})
+});
+function levelup(){
+    level++;
+    h2.innerText=`level Up ${level}`;
+    let randomx=Math.floor(Math.random()*3);
+    let randomy=btns[randomx];
+    let ranbtn=document.querySelector(`.${randomy}`);
+    console.log(randomx);
+    console.log(randomy);
+    console.log(ranbtn);
+    btnflash(ranbtn);
+}
+function btnflash(btn){
+    btn.classList.add('flash');
+    setTimeout(function(){
+        btn.classList.remove('flash');
+    },300);
+};
+// function gameflash(btn){
+//     btn.classList.add('flash');
+//     setTimeout(function(){
+//         btn.classList.remove('flash');
+//     },300);
+// };
+function userflash(btn){
+    btn.classList.add('userflash');
+    setTimeout(function(){
+        btn.classList.remove('userflash');
+    },300);
+};
+function btnpress(){
+console.log(this);
+let btn=this;
+userflash(btn);
+
+};
+let allbtns= document.querySelectorAll('.btn');
+for(btn of allbtns){
+    btn.addEventListener('click',btnpress);
+};
